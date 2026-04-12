52535fd851b74e51747e706f2ab02ac3ae3e649b
Ahmad yaseen <ahmadyasee6789@gmail.com>
2026-04-12 14:39:26 +0500

    add procfile

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print(f"Database URL: {DATABASE_URL}")  # Debugging line to check if the URL is loaded correctly
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
